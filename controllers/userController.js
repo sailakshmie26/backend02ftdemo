ba8e0cb6f827ee40174fd4c18cf649b0c553a578
Your NameSailakshmy <you@example.comsailakshmys26@gmail.com>
2026-06-23 00:37:38 +0530

    updations added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,25 @@
 import users from "../models/userModel.js";
 
-export const registerUser = () => {
+export const registerUser = (req,res) => {
+   const {username, password} = req.body;
+   if(!username || !password){
+      return res.status(400).json({message:"All fields are required"})
+   }
 
+   const userExists = users.find((user) => user.username === username)
+   if(userExists){
+      return res.status(400).json({message:"User already exists."})
+   }
+
+   const newUser = {
+      id:users.length+1;
+      username,
+      password
+   }
+   users.push(newUser)
+   res.status(201).json({message:"User registered."})
 };
 
 export const loginUser = () => {
-    
+   
 }
